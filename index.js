0dca23232541b3cf4a22a7541593136a4801d1b3
milorad996 <mips96.1@gmail.com>
2024-09-01 13:27:34 +0200

    Simulacija utakmica u grupnoj fazi i prikaz rezulata i redosleda timova u tabelama

--- index.js
+++ index.js
@@ -65,3 +65,89 @@ function simulateGame(team1, team2, form1, form2) {
         }
     };
 }
+
+
+function simulateGroupGames(group) {
+    const results = [];
+    const standings = {};
+
+    group.forEach(team => {
+        standings[team.ISOCode] = {
+            wins: 0,
+            losses: 0,
+            pointsScored: 0,
+            pointsConceded: 0,
+            points: 0
+        };
+    });
+
+    for (let i = 0; i < group.length; i++) {
+        for (let j = i + 1; j < group.length; j++) {
+            const team1 = group[i].ISOCode;
+            const team2 = group[j].ISOCode;
+            const form1 = calculateInitialForm(exibitions[team1] || []);
+            const form2 = calculateInitialForm(exibitions[team2] || []);
+            const gameResult = simulateGame(team1, team2, form1, form2);
+
+            results.push(`${gameResult.team1.name} - ${gameResult.team2.name}: ${gameResult.team1.points}:${gameResult.team2.points}`);
+
+            standings[team1].pointsScored += gameResult.team1.points;
+            standings[team1].pointsConceded += gameResult.team2.points;
+            standings[team2].pointsScored += gameResult.team2.points;
+            standings[team2].pointsConceded += gameResult.team1.points;
+
+            if (gameResult.team1.points > gameResult.team2.points) {
+                standings[team1].wins += 1;
+                standings[team1].points += 2;
+                standings[team2].losses += 1;
+                standings[team2].points += 1;
+            } else {
+                standings[team2].wins += 1;
+                standings[team2].points += 2;
+                standings[team1].losses += 1;
+                standings[team1].points += 1;
+            }
+        }
+    }
+    return { results, standings };
+}
+
+function rankTeams(standings) {
+    return Object.keys(standings).map(team => ({
+        team,
+        ...standings[team],
+        pointDifference: standings[team].pointsScored - standings[team].pointsConceded
+    })).sort((a, b) => {
+        if (b.points !== a.points) return b.points - a.points;
+        if (b.pointDifference !== a.pointDifference) return b.pointDifference - a.pointDifference;
+        return b.pointsScored - a.pointsScored;
+    });
+}
+
+function simulateTournament() {
+    const groupResults = {};
+    const finalStandings = {};
+
+    Object.keys(groups).forEach(groupKey => {
+        console.log(`Grupa ${groupKey}:`);
+        const { results, standings } = simulateGroupGames(groups[groupKey]);
+        groupResults[groupKey] = results;
+        finalStandings[groupKey] = rankTeams(standings);
+
+        results.forEach(result => console.log(result));
+        console.log();
+    });
+
+    console.log('Konačan plasman u grupama:');
+    Object.keys(finalStandings).forEach(groupKey => {
+        console.log(`Grupa ${groupKey}:`);
+        finalStandings[groupKey].forEach((team, idx) => {
+            console.log(`${idx + 1}. ${team.team} - pobede/porazi: ${team.wins}/${team.losses}, bodovi: ${team.points}, koševi: ${team.pointsScored}/${team.pointsConceded}, koš razlika: ${team.pointDifference}`);
+        });
+        console.log();
+    });
+
+    return finalStandings;
+}
+
+console.log(simulateTournament());
